1963aa1f97055c0f05788fd2ff13efcd299fc517
Old Fox <old.fox@mail.ru>
2021-06-24 00:19:04 +0500

    refactoring to BigInteger (very simple code)

--- Number Base Converter/task/src/converter/Main.kt
+++ Number Base Converter/task/src/converter/Main.kt
@@ -1,68 +1,24 @@
 package converter
 
-import kotlin.math.pow
-
-fun getInt(vText: String): Int {
-    print(vText)
-    return readLine()!!.toInt()
-}
+import java.math.BigInteger
 
 fun getString(vText: String): String {
     print(vText)
     return readLine()!!
 }
 
-
 class NumberConverter() {
-    private val abc = "ABCDEF"
-
-    private fun toStr(numb: Int, sys: Int) = if (sys <= 10 || numb < 10) numb.toString()
-    else abc[numb - 10].toString()
-
-    private fun toNumb(str: String, sys: Int) = if (sys <= 10 || str !in abc) str.toInt()
-    else 10 + abc.indexOf(str)
-
-    private fun convertFrom(numb: Int, sys: Int): String {
-        var res = numb
-        var out = ""
-        while (res != 0) {
-            out = toStr(res % sys, sys) + out
-            res /= sys
-        }
-        return out
-    }
-
-    private fun convertTo(str: String, sys: Int): String {
-        var res = 0
-        for ((a, i) in (str.lastIndex downTo 0).withIndex()) {
-            res += toNumb(str[i].toString(), sys) * sys.toDouble().pow(a).toInt()
-        }
-        return res.toString()
-    }
-
-    private fun getMenuConvertFrom(){
-        val numb = getInt("Enter number in decimal system: ")
-        val sys = getInt("Enter target base: ")
-
-        val result = convertFrom(numb, sys)
-        println("Conversion result: $result")
-    }
-
-    private fun getMenuConvertTo(){
-        val str = getString("Enter source number: ")
-        val sys = getInt("Enter source base: ")
-        val result = convertTo(str.toUpperCase(), sys)
-        println("Conversion to decimal result: $result")
-    }
+    fun convert(strBi: String, fromBase: Int, toBase: Int) = BigInteger(strBi, fromBase).toString(toBase)
 
     fun getMenu() {
         while (true) {
-            print("Do you want to convert /from decimal or /to decimal? (To quit type /exit): ")
-            val action = readLine()!!
-            when (action) {
-                "/from" -> getMenuConvertFrom()
-                "/to" -> getMenuConvertTo()
-                "/exit" -> return
+            var inputStr = getString("Enter two numbers in format: {source base} {target base} (To quit type /exit) ")
+            if (inputStr == "/exit") return
+
+            val (fromBase, toBase) = inputStr.split(' ')
+            inputStr = getString("Enter number in base $fromBase to convert to base $toBase (To go back type /back) ")
+            if (inputStr != "/back") {
+                println("Conversion result: ${convert(inputStr, fromBase.toInt(), toBase.toInt())}")
             }
             println("")
         }
