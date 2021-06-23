e7142e5bb97e8a1c583e2370d3fae71057edccc2
Old Fox <old.fox@mail.ru>
2021-06-24 00:30:14 +0500

    refactoring to BigInteger (very simple code) working solution!

--- Number Base Converter/task/src/converter/Main.kt
+++ Number Base Converter/task/src/converter/Main.kt
@@ -10,16 +10,21 @@ fun getString(vText: String): String {
 class NumberConverter() {
     fun convert(strBi: String, fromBase: Int, toBase: Int) = BigInteger(strBi, fromBase).toString(toBase)
 
-    fun getMenu() {
-        while (true) {
-            var inputStr = getString("Enter two numbers in format: {source base} {target base} (To quit type /exit) ")
-            if (inputStr == "/exit") return
-
-            val (fromBase, toBase) = inputStr.split(' ')
-            inputStr = getString("Enter number in base $fromBase to convert to base $toBase (To go back type /back) ")
+    fun getMenuSecond(numbers: String) {
+        while(true) {
+            val (fromBase, toBase) = numbers.split(' ')
+            val inputStr = getString("Enter number in base $fromBase to convert to base $toBase (To go back type /back) ")
             if (inputStr != "/back") {
                 println("Conversion result: ${convert(inputStr, fromBase.toInt(), toBase.toInt())}")
-            }
+            } else return
+        }
+    }
+
+    fun getMenu() {
+        while (true) {
+            val inputStr = getString("Enter two numbers in format: {source base} {target base} (To quit type /exit) ")
+            if (inputStr == "/exit") return
+            getMenuSecond(inputStr)
             println("")
         }
     }
